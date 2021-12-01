38e28490b123973f29a60c8a16a9667100f4cc82
Eli Lipsitz <eli.lipsitz@gmail.com>
2021-12-01 11:11:52 -0600

    day01: part 1

--- src/day01.rs
+++ src/day01.rs
@@ -1,3 +1,9 @@
+fn parse(input: &str) -> Vec<i32> {
+    input.lines().map(|x| x.parse::<i32>().unwrap()).collect()
+}
+
 pub fn part1(input: String) {
-    println!("{}", input.chars().rev().collect::<String>());
+    let input = parse(&input);
+    let count = input.windows(2).filter(|x| x[1] > x[0]).count();
+    println!("Increases: {}", count);
 }
