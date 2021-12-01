c3fccbc33679a5c8138bbfd1f9d2e47454c661a4
Eli Lipsitz <eli.lipsitz@gmail.com>
2021-12-01 11:15:41 -0600

    day01: part 2

--- src/day01.rs
+++ src/day01.rs
@@ -7,3 +7,10 @@ pub fn part1(input: String) {
     let count = input.windows(2).filter(|x| x[1] > x[0]).count();
     println!("Increases: {}", count);
 }
+
+pub fn part2(input: String) {
+    let input = parse(&input);
+    let groups: Vec<i32> = input.windows(3).map::<i32, _>(|x| x.iter().sum()).collect();
+    let count = groups.windows(2).filter(|x| x[1] > x[0]).count();
+    println!("Increases: {}", count);
+}
