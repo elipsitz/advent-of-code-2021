1cca01a6c7d3682c9c48d5b53c3e1fd2531e4788
Eli Lipsitz <eli.lipsitz@gmail.com>
2021-12-03 01:06:13 -0600

    day03: part 2

--- src/day03.rs
+++ src/day03.rs
@@ -8,9 +8,7 @@ fn parse(input: &str) -> Vec<u32> {
         .collect()
 }
 
-pub fn part1(input: String) {
-    let input = parse(&input);
-
+fn calculate_gamma_epsilon(input: &[u32]) -> (u32, u32) {
     // Count of 1 bits in each position.
     let counts = (0..BITS).map(|index| {
         input
@@ -23,14 +21,44 @@ pub fn part1(input: String) {
     let gamma = counts
         .enumerate()
         .map(|(i, count)| {
-            let bit = (count >= (input.len() / 2)) as u32;
+            let bit = (2 * count >= input.len()) as u32;
             bit << i
         })
         .fold(0, |a, b| (a | b));
-
     let epsilon = (!gamma) & MASK;
 
+    (gamma, epsilon)
+}
+
+pub fn part1(input: String) {
+    let input = parse(&input);
+
+    let (gamma, epsilon) = calculate_gamma_epsilon(&input);
     println!("Combined: {}", gamma * epsilon);
 }
 
-pub fn part2(_input: String) {}
+pub fn part2(input: String) {
+    let input = parse(&input);
+
+    let mut oxygen = input.clone();
+    for bit in (0..BITS).rev() {
+        let (gamma, _) = calculate_gamma_epsilon(&oxygen);
+        oxygen.retain(|&x| ((x >> bit) & 1) == ((gamma >> bit) & 1));
+        if oxygen.len() == 1 {
+            break;
+        }
+    }
+
+    let mut co2 = input.clone();
+    for bit in (0..BITS).rev() {
+        let (_, epsilon) = calculate_gamma_epsilon(&co2);
+        co2.retain(|&x| ((x >> bit) & 1) == ((epsilon >> bit) & 1));
+        if co2.len() == 1 {
+            break;
+        }
+    }
+
+    assert_eq!(oxygen.len(), 1);
+    assert_eq!(co2.len(), 1);
+    println!("combined: {}", oxygen[0] * co2[0]);
+}
